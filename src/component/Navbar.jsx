eafe0f3d435b99f1bd92bd287425dc009ac6dfa0
Anmol Git <Anmol>
2024-01-04 12:00:30 +0530

    Initialize project using Create React App

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div>logo</div>
+        <div>
+            <spa></spa>
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Navbar
